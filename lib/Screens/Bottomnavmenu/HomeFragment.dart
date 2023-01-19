7d1bdfc1e5efaadda89608261fb67fadd6ea240f
sakchaisrim <45826964+sakchaisrim@users.noreply.github.com>
2023-01-19 16:56:55 +0700

    2nd

--- lib/Screens/Bottomnavmenu/HomeFragment.dart
+++ lib/Screens/Bottomnavmenu/HomeFragment.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:smart_news/Screens/Bottomnavmenu/MenuList.dart';
+import 'package:smart_news/Themes/color.dart';
 
 class HomeFragment extends StatefulWidget {
 
@@ -9,8 +11,94 @@ class HomeFragment extends StatefulWidget {
 class _HomeFragmentState extends State<HomeFragment> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home', style: TextStyle(fontSize: 24),),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: const Text('Smart News', style: TextStyle(color: textColorSecondary),),
+        actions: [
+          IconButton(
+            onPressed: (){}, 
+            icon: Icon(Icons.video_call)
+          )
+        ],
+      ),
+      drawer: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: googleColor
+        ),
+        child: Drawer(
+          child: ListView(
+            children: [
+              UserAccountsDrawerHeader(
+                accountName: Text('Sakchai Srimakorn'), 
+                accountEmail: Text('sakchaisr@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                  //
+                ),
+                decoration: BoxDecoration(
+                  color: googleColor,
+                ),
+              ),
+              MenuList(
+                micon: Icons.login, 
+                mname: 'Login', 
+                onTap: (){
+                  //
+                }
+              ),
+              MenuList(
+                micon: Icons.new_releases, 
+                mname: 'Latest news', 
+                onTap: (){
+                  //
+                }
+              ),
+              MenuList(
+                micon: Icons.category, 
+                mname: 'Category', 
+                onTap: (){
+                  //
+                }
+              ),
+              MenuList(
+                micon: Icons.video_collection, 
+                mname: 'Video List', 
+                onTap: (){
+                  //
+                }
+              ),
+              MenuList(
+                micon: Icons.info, 
+                mname: 'About', 
+                onTap: (){
+                  //
+                }
+              ),
+              MenuList(
+                micon: Icons.settings, 
+                mname: 'Setting', 
+                onTap: (){
+                  //
+                }
+              ),
+              MenuList(
+                micon: Icons.logout, 
+                mname: 'Logout', 
+                onTap: (){
+                  //
+                }
+              ),
+            ],
+          ),
+        ),
+      ),
+      
+
+
+
+      body: Container(),
     );
   }
 }
